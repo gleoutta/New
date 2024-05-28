92a510dad6406ee3420709a49e24d562ddf6804f
Gabriel Santos <117317317+gleoutta@users.noreply.github.com>
2024-05-28 14:41:08 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+function myfunction1(){
+    document.getElementById("demo").innerHTML = "BIBLIA" 
+}
+
+function myfunction2(){
+    document.getElementById('demo').style.fontSize='14px' 
+}
+
+function myfunction3(){
+    document.getElementById("demo").innerHTML = "Deus"
+}
+
+function myfunction4(){
+    document.getElementById('conta').innerHTML = 2 + 8;
+}
+ 
